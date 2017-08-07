f25247d9d0ab17c4834ec6cb92afaebb85dcc932
ykubot <yuichikubota74@gmail.com>
2017-08-08 01:02:45 +0900

    NavigationBarをAndroid対応

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Text, Platform } from 'react-native';
 import { Router, Scene, Actions } from 'react-native-router-flux';
 import CryptCurrencyList from './components/CryptCurrencyList';
 import CryptCurrencyDetail from './components/CryptCurrencyDetail';
+import settingsModal from './components/settingsModal';
+import CloseButton from './components/common/CloseButton';
+import {HEADER_HEIGHT_IOS, HEADER_HEIGHT_ANDROID} from './constants/Constants';
 
 const RouterComponent = () => {
     return(
@@ -10,7 +14,9 @@ const RouterComponent = () => {
                 <Scene
                     key="cryptCurrencyList"
                     component={CryptCurrencyList}
-                    title="CC checker"
+                    title="CC Checker"
+                    onRight={() => Actions.settingsModal()}
+                    rightTitle="Settings"
                     initial
                 />
 
@@ -19,20 +25,41 @@ const RouterComponent = () => {
                     component={CryptCurrencyDetail}
                     title="Coin Detail"
                 />
+
             </Scene>
+            <Scene
+                key="settingsModal"
+                direction="vertical"
+                renderBackButton={() => <CloseButton />}
+                component={settingsModal}
+                schema="modal"
+                title="Settings"
+            />
         </Router>
     );
 };
 
 const styles = {
     navBarStyle: {
-        backgroundColor: '#00B7C3'
+        flex: 1,
+        backgroundColor: '#00B7C3',
+        paddingTop: Platform.select({
+            ios: 0,
+            android: 20
+        }),
+        height: Platform.select({
+            ios: HEADER_HEIGHT_IOS,
+            android: HEADER_HEIGHT_ANDROID
+        }),
     },
     titleStyle: {
         color: '#FFFFFF'
     },
     sceneStyle: {
-        paddingTop: 65
+        paddingTop: Platform.select({
+            ios: HEADER_HEIGHT_IOS,
+            android: HEADER_HEIGHT_ANDROID
+        }),
     }
 }
 
